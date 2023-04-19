11a162a21a7a34accebc1ce720318fc3d6276a3d
Diego Mendoza <119355955+zdzyy@users.noreply.github.com>
2023-04-19 14:43:36 -0600

    some sections

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 import Header from './componentes/Header/Header';
 import Formulario from './componentes/Formulario/Formulario';
 import MiOrg from './componentes/MiOrg';
+import Equipo from './componentes/Equipo';
 
 function App() {
-  const [mostrarFormulario, actualizarMostrar] = useState(true)
+  const [mostrarFormulario, actualizarMostrar] = useState(false)
 
   //Ternario --> condicion ? seMuestra : noSeMuestra
   // condicion && seMuestra
@@ -14,12 +15,58 @@ function App() {
     actualizarMostrar(!mostrarFormulario)
   }
 
+  //Lista de equipos
+  const equipos = [
+    {
+      titulo:"Programacion",
+      colorPrimario: "#57C278",
+      colorSecundario: "#D9F7E9"
+    },
+    { 
+      titulo:"Front End",
+      colorPrimario: "#82CFFA",
+      colorSecundario: "#E8F8FF"
+    },
+    { 
+      titulo:"Data Science",
+      colorPrimario: "#A6D157",
+      colorSecundario: "#F0F8E2"
+    },
+    { 
+      titulo:"Devops",
+      colorPrimario: "#E06B69",
+      colorSecundario: "#FDE7E8"
+    },
+    { 
+      titulo:"Ux y Diseño",
+      colorPrimario: "#DB6EBF",
+      colorSecundario: "#FAE9F5"
+    },
+    { 
+      titulo:"Movil",
+      colorPrimario: "#FFBA05",
+      colorSecundario: "#FFF5D9"
+    },
+    { 
+      titulo:"Innovacion y Gestion",
+      colorPrimario: "#FF8A29",
+      colorSecundario: "#FFEEDF"
+    },
+]
+
   return (
     <div>
       <Header />
       {/* mostrarFormulario ? <Formulario /> : <></> */}
       {mostrarFormulario && <Formulario />}
       <MiOrg cambiarMostrar={cambiarMostrar}/>
+      
+      {
+        equipos.map( () => {
+          return <Equipo />
+        } )
+      }
+
     </div>
   );
 }
